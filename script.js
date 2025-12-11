f569dc39b2d8d8a9836abe62ba4aa774f0b98422
pragatiagrawal69 <your.email@example.com>
2025-12-11 14:00:11 +0530

    fetching data

--- script.js
+++ script.js
@@ -1,64 +1,66 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    const API_KEY = "d1be427e961de0b6269f2a8760e3987d"
-    let interval = null 
-
-    const weatherContent = document.querySelector('#weather-content')
-    const loading = document.querySeletor('#loading')
-    const errorMsg = document.querySeletor('#error-msg')
-    const error = document.querySeletor('#error')
-    const searchForm = document.querySeletor('#search-form')
-    const cityInput= document.querySeletor('#city-input')
-    const closeBtn= document.querySeletor('#close-btn')
-    const animationCon= document.querySeletor('#animation-con')
-    const cityName= document.querySeletor('#city-name')
-    const currentDate= document.querySeletor('#current-date')
-    const currentTime= document.querySeletor('#current-time')
-    const currentTemp= document.querySeletor('#current-temp')
-    const currentWeather= document.querySeletor('#current-wheather')
-    const icon= document.querySeletor('#icon')
-    const forecastCon= document.querySeletor('#forecast-con')
+const API_KEY = "d1be427e961de0b6269f2a8760e3987d"
+const weatherContent = document.querySelector('#weather-content')
+const loading = document.querySelector('#loading')
+const errorMsg = document.querySelector('#error-msg')
+const error = document.querySelector('#error')
+const searchForm = document.querySelector('#search-form')
+const cityInput = document.querySelector('#city-input')
+const closeBtn = document.querySelector('#close-btn')
+const searchBtn = document.querySelector('#search-btn')
+const animationCon = document.querySelector('#animation-con')
+const cityName = document.querySelector('#city-name')
+const currentDate = document.querySelector('#current-date')
+const currentTime = document.querySelector('#current-time')
+const currentTemp = document.querySelector('#current-temp')
+const currentWeather = document.querySelector('#current-wheather')
+const icon = document.querySelector('#icon')
+const forecastCon = document.querySelector('#forecast-con')
 
 
-    const fetchWeather = async ({lat,lon,city})=>{
-        showLoading()
-        if(clearInterval) clearInterval
-        (interval)
-        try {
-            if(!API_KEY) throw new Error ("OpenWeatherMap API Key is missing.")
-            let latitude = lat
-            let longitude = lon
 
-            if(city){
-                const Url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`
-                const getResponce = await fetch(Url)
-                if(!getResponce.ok) throw new Error(`Could not find data for "${city}".`)
-                    const data = await getResponce.json()
-                if(data.length ===0) throw new Error (`Could not find location data for "${city}".`)
-                    latitude = data[0].lat
-                    longitude= data[0].lon
-            }
+const fetchWeather = async (city) => {
+    showLoading()
+    try {
+        if (!API_KEY) {throw new Error("OpenWeatherMap API Key is missing.")}
+
+        if (city) {
+            const Url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`
+            const getResponce = await fetch(Url)
+            // console.log(getResponce);
+            if (!getResponce.ok) throw new Error(`Could not find data for "${city}".`)
+            const data = await getResponce.json()
+            console.log(data)
+            if (data.length === 0) throw new Error(`Could not find location data for "${city}".`)
+            let latitude = data[0].lat
+            let longitude = data[0].lon
             const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
-            const forecastUrl = `api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+            // const forecastUrl = `api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+            const weatherResponce = await fetch(weatherUrl)
+            const data1 = await weatherResponce.json()
+            // console.log(weatherResponce)
+            console.log(data1)
 
-        } catch (error) {
-            console.error('weather data fetch error:',error);
-            showError(error.msg)
-        } finally {
-            hideloading()
         }
+
+    } catch (error) {
+        console.error('weather data fetch error:', error);
+        showError(error.msg)
+    } finally {
+        hideloading()
     }
-    const showLoading = () =>{
-        loading.classList.remove('hidden')
-        loading.classList.add('flex')
-    }
-    const hideloading = ()=>{
-        loading.classList.add('hidden')
-        loading.classList.remove('flex')
-        weatherContent.classList.add('opacity-0')
-    }
-    const showError = (msg) =>{
-        errorMsg.textContent = Msg
-        error.classList.remove('hidden')
-    }
-})
+}
+const showLoading = () => {
+    loading.classList.remove('hidden')
+    loading.classList.add('flex')
+}
+const hideloading = () => {
+    loading.classList.add('hidden')
+    loading.classList.remove('flex')
+}
+const showError = (msg) => {
+    errorMsg.textContent = Msg
+    error.classList.remove('hidden')
+    error.classList.add('flex')
+}
+fetchWeather('raipur')
 
